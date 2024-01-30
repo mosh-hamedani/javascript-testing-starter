41cccbd6178810cd065bf2f7484de70766b1008b
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-01-30 15:04:18 +0100

    update

--- src/intro.js
+++ src/intro.js
@@ -1,14 +1,12 @@
 // Lesson: Writing your first tests
 export function max(a, b) {
-  if (a > b) return a;
-  else if (b > a) return b;
-  return a;
+  return a > b ? a : b;
 }
 
 // Exercise
 export function fizzBuzz(n) {
-  if (n % 3 === 0 && n % 5 === 0) return 'FizzBuzz';
-  if (n % 3 === 0) return 'Fizz';
-  if (n % 5 === 0) return 'Buzz';
+  if (n % 3 === 0 && n % 5 === 0) return "FizzBuzz";
+  if (n % 3 === 0) return "Fizz";
+  if (n % 5 === 0) return "Buzz";
   return n.toString();
 }
--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,11 +1,15 @@
 import { describe, it, test, expect } from "vitest";
-
+import { max } from "../src/intro";
 describe("max", () => {
   it("should return the first argument if it is greater.", () => {
-    //Arrange
-    const a = 2;
-    const b = 1;
-    //Act
-    //Assert
+    expect(max(2, 1)).toBe(2);
+  });
+
+  it("should return the second argument if it is greater.", () => {
+    expect(max(1, 2)).toBe(2);
+  });
+
+  it("should return the first argument if they are equal.", () => {
+    expect(max(1, 1)).toBe(1);
   });
 });
