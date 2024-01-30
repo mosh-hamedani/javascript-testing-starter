aae1ae0df2416656ed042910d84ba801e623769a
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-01-30 13:27:14 +0100

    describle + it functions

---
+++ tests/intro.test.js
@@ -0,0 +1,11 @@
+import { describe, it, test, expect } from "vitest";
+
+describe("max", () => {
+  it("should return the first argument if it is greater.", () => {
+    //Arrange
+    const a = 2;
+    const b = 1;
+    //Act
+    //Assert
+  });
+});
